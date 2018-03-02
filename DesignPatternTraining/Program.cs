3fe8d2377342b42b07bf7e7a3f3a17a1c24bfc04
Mathieu Scolas <mathieu.scolas@gmail.com>
2018-03-02 18:23:09 +0100

    initial commit

---
+++ DesignPatternTraining/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPatternTraining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
