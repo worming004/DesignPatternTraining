29b210dc815b2a1f007f7e954d698634f45e11a2
Mathieu Scolas <mathieu.scolas@gmail.com>
2018-03-02 21:50:25 +0100

    DO NOT WORKS. The example choosed is not efficient

---
+++ Decorator/DP/OrigamiSheetDecorator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Decorator.Situation;
+
+namespace Decorator.DP
+{
+    public abstract class OrigamiSheetDecorator : ISheet
+    {
+        private ISheet _sheet;
+        
+        OrigamiSheetDecorator(ISheet sheet)
+        {
+            this._sheet = sheet;
+        }
+
+        public Render()
+        {
+            return this._sheet.Render();
+        }
+
+        public abstract Origami MakeOrigami();
+    }
+
+    public 
+}
---
+++ Decorator/Situation/Sheet.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Decorator.Situation
+{
+    public interface ISheet
+    {
+        int Width { get; }
+        int Height { get; }
+    }
+
+
+    public class A4Sheet : ISheet
+    {
+        public int Width { get => 16; }
+        public int Height { get => 30; }
+
+
+        private List<ISign<Object>> _content = new List<ISign<Object>>();
+
+
+        public void AddSign(ISign<Object> sign)
+        {
+            this._content.Add(sign);
+        }
+
+        public IEnumerable<Object> RenderAll()
+        {
+            var result = new List<Object>();
+            foreach (var c in _content)
+            {
+                result.Add(c.Render());
+            }
+            return result;
+        }
+    }
+}
